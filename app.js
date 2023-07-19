1dc583a748a047bc273dceee86a1068a2815da8a
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-07-18 22:17:46 -0600

    46: stdin - stdout - Readline

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 require('colors');
-console.clear();
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 const main = async () => {
-    console.log('Hola mundo');
+    mostrarMenu();
+    pausa();
 };
 
 main();
