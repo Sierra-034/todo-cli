840a4512fc602de4e4ca060163e82ae88d46b20c
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-07-20 01:12:25 -0600

    47: Repetir el menú de forma infinita

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ require('colors');
 const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 const main = async () => {
-    mostrarMenu();
-    pausa();
+    let opt = '';
+    do {
+        opt = await mostrarMenu();
+        console.log({opt});
+
+        if (opt !== '0') await pausa();
+    } while(opt !== '0');
 };
 
 main();
