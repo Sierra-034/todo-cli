d5eed652c26ad86515fa04746843793109d6ec5a
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-07-24 13:18:51 -0600

    50: Opciones del menú interactivo

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 require('colors');
-const { inquirerMenu } = require('./helpers/inquirer');
-const { pausa } = require('./helpers/mensajes');
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
 
 const main = async () => {
     let opt = '';
     do {
-        const {opcion: opt} = await inquirerMenu();
-        console.log(typeof(opt));
-    } while(opt !== '0');
+        opt = await inquirerMenu();
+        if (opt != 0) await pausa();
+    } while(opt != 0);
 };
 
 main();
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,25 +1,38 @@
 require('colors');
 const inquirer = require('inquirer');
 
-const preguntas = [
-    {
-        type: 'list',
-        name: 'opcion',
-        message: '¿Qué desea hacer?',
-        choices: ['opt1', 'opt2', 'opt3'],
-    },
-]
+const preguntasMenuPrincipal = {
+    type: 'list',
+    name: 'opcion',
+    message: '¿Qué desea hacer?',
+    choices: [
+        { value: 1, name: '1. Crear tarea' },
+        { value: 2, name: '2. Listar tareas' },
+        { value: 3, name: '3. Listar tareas completadas' },
+        { value: 4, name: '4. Listar tareas pendientes' },
+        { value: 5, name: '5. Completar tarea(s)' },
+        { value: 6, name: '6. Borrar tarea' },
+        { value: 0, name: 'Salir' },
+    ],
+};
 
 const inquirerMenu = async () => {
-    console.clear();
-    console.log('============================='.green);
-    console.log('    Seleccione una opción'.green);
-    console.log('=============================\n'.green);
-
-    const opt = await inquirer.prompt(preguntas);
-    return opt;
+    const { opcion } = await inquirer.prompt(preguntasMenuPrincipal);
+    return opcion;
 };
+
+const preguntasPausa = {
+    type: 'input',
+    name: 'pausaAnswer',
+    message: `Presione ${'Enter'.green} para continuar`,
+};
+
+const pausa = async () => {
+    const { pausaAnswer } = await inquirer.prompt(preguntasPausa);
+    return pausaAnswer;
+}
 
 module.exports = {
     inquirerMenu,
+    pausa,
 }
