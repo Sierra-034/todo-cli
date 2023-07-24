b8ecf798f8bf271c683506da0fbbe2460db831d1
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-07-24 14:24:02 -0600

    52: Crear y listar tareas

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,38 +1,53 @@
 require('colors');
 const inquirer = require('inquirer');
 
-const preguntasMenuPrincipal = {
-    type: 'list',
-    name: 'opcion',
-    message: '¿Qué desea hacer?',
-    choices: [
-        { value: 1, name: '1. Crear tarea' },
-        { value: 2, name: '2. Listar tareas' },
-        { value: 3, name: '3. Listar tareas completadas' },
-        { value: 4, name: '4. Listar tareas pendientes' },
-        { value: 5, name: '5. Completar tarea(s)' },
-        { value: 6, name: '6. Borrar tarea' },
-        { value: 0, name: 'Salir' },
-    ],
-};
-
 const inquirerMenu = async () => {
+    const preguntasMenuPrincipal = {
+        type: 'list',
+        name: 'opcion',
+        message: '¿Qué desea hacer?',
+        choices: [
+            { value: 1, name: '1. Crear tarea' },
+            { value: 2, name: '2. Listar tareas' },
+            { value: 3, name: '3. Listar tareas completadas' },
+            { value: 4, name: '4. Listar tareas pendientes' },
+            { value: 5, name: '5. Completar tarea(s)' },
+            { value: 6, name: '6. Borrar tarea' },
+            { value: 0, name: 'Salir' },
+        ],
+    };
     const { opcion } = await inquirer.prompt(preguntasMenuPrincipal);
     return opcion;
 };
 
-const preguntasPausa = {
-    type: 'input',
-    name: 'pausaAnswer',
-    message: `Presione ${'Enter'.green} para continuar`,
-};
 
 const pausa = async () => {
+    const preguntasPausa = {
+        type: 'input',
+        name: 'pausaAnswer',
+        message: `Presione ${'Enter'.green} para continuar`,
+    };
     const { pausaAnswer } = await inquirer.prompt(preguntasPausa);
     return pausaAnswer;
+};
+
+const leerInput = async (message) => {
+    const question = {
+        type: 'input',
+        name: 'desc',
+        message,
+        validate: (value) => {
+            if (value.length === 0) return 'Por favor, ingresa un valor';
+            return true;
+        }
+    };
+
+    const {desc} = await inquirer.prompt(question);
+    return desc;
 }
 
 module.exports = {
     inquirerMenu,
     pausa,
+    leerInput,
 }
--- models/tareas.js
+++ models/tareas.js
@@ -1,6 +1,13 @@
+const Tarea = require('./tarea');
+
 class Tareas {
     constructor() {
         this._listado = new Map();
+    }
+
+    crearTarea(desc = '') {
+        const tarea = new Tarea(desc);
+        this._listado.set(tarea.id, tarea);
     }
 }
 
