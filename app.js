01b96d3ed76fb11a7192de762575a949a240d2b3
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-07-24 12:37:58 -0600

    49: Construir el menú interactivo - inquirer

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 require('colors');
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
+const { inquirerMenu } = require('./helpers/inquirer');
+const { pausa } = require('./helpers/mensajes');
 
 const main = async () => {
     let opt = '';
     do {
-        opt = await mostrarMenu();
-        console.log({opt});
-
-        if (opt !== '0') await pausa();
+        const {opcion: opt} = await inquirerMenu();
+        console.log(typeof(opt));
     } while(opt !== '0');
 };
 
---
+++ helpers/inquirer.js
@@ -0,0 +1,25 @@
+require('colors');
+const inquirer = require('inquirer');
+
+const preguntas = [
+    {
+        type: 'list',
+        name: 'opcion',
+        message: '¿Qué desea hacer?',
+        choices: ['opt1', 'opt2', 'opt3'],
+    },
+]
+
+const inquirerMenu = async () => {
+    console.clear();
+    console.log('============================='.green);
+    console.log('    Seleccione una opción'.green);
+    console.log('=============================\n'.green);
+
+    const opt = await inquirer.prompt(preguntas);
+    return opt;
+};
+
+module.exports = {
+    inquirerMenu,
+}
